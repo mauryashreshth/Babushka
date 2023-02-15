80dc6fa16c5d085521bbc2f233572623046adc95
mauryashreshth <86547237+mauryashreshth@users.noreply.github.com>
2023-02-15 19:40:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,63 @@
-const searchBox = document.getElementById("search-box");
-const searchButton = document.getElementById("search-button");
-const weatherInfo = document.getElementById("weather-info");
+window.onload = function () {
+    document.querySelector('.container').classList.add('active');
+    document.getElementById('search-box').value = '';
+    const searchBox = document.getElementById("search-box");
+    const searchButton = document.getElementById("search-button");
+    const weatherInfo = document.getElementById("weather-info");
+    // function showMessage(duration) {
+    //     const messageElement = document.getElementById("loading-icon-container");
 
-searchButton.addEventListener("click", () => {
-    const cityName = searchBox.value;
-    const apiKey = "2fb2c57a6255bd20f07aa856d18ef498"; // replace with your API key
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+    //     messageElement.style.display = "block";
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            if (data.main) {
-                const temperature = data.main.temp;
-                const description = data.weather[0].description;
-                const iconCode = data.weather[0].icon;
-                const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-
-                weatherInfo.innerHTML = `
+    //     // Hide the message element after the specified duration
+    //     setTimeout(function () {
+    //         messageElement.style.display = "none";
+    //     }, duration);
+    // }
+    // function hideMessage() {
+    //     const messageElement = document.getElementById("loading-icon-container");
+    //     messageElement.style.display = "none";
+    // }
+    searchButton.addEventListener("click", () => {
+        // showMessage(800);
+        const cityName = searchBox.value;
+        const apiKey = "2fb2c57a6255bd20f07aa856d18ef498"; // replace with your API key
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.main) {
+                    const temperature = Math.round(data.main.temp);
+                    const description = data.weather[0].description;
+                    const iconCode = data.weather[0].icon;
+                    const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+                    weatherInfo.innerHTML = `
                     <h2>${cityName}</h2>
                     <div>
                         <img src="${iconUrl}" alt="${description}">
-                        <span>${temperature}&deg;C</span>
+                        <p>${temperature}&deg;C</p>
+                        <p>${description}</p>
                     </div>
-                    <p>${description}</p>
                 `;
-            } else {
-                weatherInfo.innerHTML = `<p>Error: Unable to get weather data for ${cityName}</p>`;
-            }
-        })
-        .catch(error => {
-            weatherInfo.innerHTML = `<p>Error: ${error.message}</p>`;
-        });
-    // fetch(url)
-    // .then(response => response.json())
-    // .then(data => {
-    //     // your code to process the data
-    // })
-    // .catch(error => {
-    //     console.log(error);
-    // });
-});
+                } else {
+                    weatherInfo.innerHTML = `<p>Error: Unable to get weather data for ${cityName}</p>`;
+                }
+            })
+            .catch(error => {
+                weatherInfo.innerHTML = `<p>Error: ${error.message}</p>`;
+            });
+        // .finally(() => {
+        //     hideMessage(); // hide loading icon when data is loaded
+        // });
+    });
+    searchBox.addEventListener('keypress', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            searchButton.click();
+        }
+    });
+    window.onbeforeunload = function () {
+        localStorage.clear();
+    }
+
+};
