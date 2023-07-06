8cd73389c8a6957939249b44175903d013c02f75
mauryashreshth <shreshthmaurya2003@gmail.com>
2023-07-06 20:29:14 +0530

    Unsplash API is integrated

--- script.js
+++ script.js
@@ -1,64 +1,83 @@
 window.onload = function () {
-    document.querySelector('.container').classList.add('active');
-    document.getElementById('search-box').value = '';
-    const searchBox = document.getElementById("search-box");
-    const searchButton = document.getElementById("search-button");
-    const weatherInfo = document.getElementById("weather-info");
-    // function showMessage(duration) {
-    //     const messageElement = document.getElementById("loading-icon-container");
+  document.querySelector(".container").classList.add("active");
+  document.getElementById("search-box").value = "";
+  const searchBox = document.getElementById("search-box");
+  const searchButton = document.getElementById("search-button");
+  const weatherInfo = document.getElementById("weather-info");
+  searchButton.addEventListener("click", () => {
+    // showMessage(800);
 
-    //     messageElement.style.display = "block";
+    const cityName = searchBox.value;
+    const apiKey = "2fb2c57a6255bd20f07aa856d18ef498"; // replace with your API key
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
-    //     // Hide the message element after the specified duration
-    //     setTimeout(function () {
-    //         messageElement.style.display = "none";
-    //     }, duration);
-    // }
-    // function hideMessage() {
-    //     const messageElement = document.getElementById("loading-icon-container");
-    //     messageElement.style.display = "none";
-    // }
-    searchButton.addEventListener("click", () => {
-        // showMessage(800);
-        const cityName = searchBox.value;
-        const apiKey = "2fb2c57a6255bd20f07aa856d18ef498"; // replace with your API key
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                if (data.main) {
-                    const temperature = Math.round(data.main.temp);
-                    const description = data.weather[0].description;
-                    const iconCode = data.weather[0].icon;
-                    const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
-                    console.log("ICON CODE : ", iconCode);
-                    weatherInfo.innerHTML = `
-                    <h2>${cityName}</h2>
-                    <div>
-                        <img src="${iconUrl}" alt="${description}">
-                        <p>${temperature}&deg;C</p>
-                        <p>${description}</p>
-                    </div>
-                `;
-                } else {
-                    weatherInfo.innerHTML = `<p>Error: Unable to get weather data for ${cityName}</p>`;
-                }
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.main) {
+          const temperature = Math.round(data.main.temp);
+          const description = data.weather[0].description;
+          const accessKey = "Sotwa99Pz-DbC9UnPQSSjbk9Udrx5ISfDpWV2ZV7o3k";
+
+          // Set the endpoint URL for the desired API request
+          const endpointURL = "https://api.unsplash.com/photos/random";
+
+          // Set the search query parameter to 'clouds'
+          const query = `${description} weather`;
+          const searchURL = `${endpointURL}?query=${query}`;
+
+          console.log(`Query name: ${query}`);
+          // Set the headers object with your access key
+          const headers = {
+            Authorization: `Client-ID ${accessKey}`,
+          };
+
+          const body = document.querySelector("body");
+
+          // Make the API request using fetch
+          fetch(searchURL, { headers })
+            .then((response) => response.json())
+            .then((imageData) => {
+              // Extract the image URL from the API response
+              const imageUrl = imageData.urls.regular;
+
+              // Set the background image of the HTML element
+              body.style.background = `url(${imageUrl}) no-repeat center center fixed`;
+              body.style.backgroundSize = "cover";
             })
-            .catch(error => {
-                weatherInfo.innerHTML = `<p>Error: ${error.message}</p>`;
+            .catch((error) => {
+              console.log(error.message);
             });
-        // .finally(() => {
-        //     hideMessage(); // hide loading icon when data is loaded
-        // });
-    });
-    searchBox.addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
-            event.preventDefault();
-            searchButton.click();
+
+          const iconCode = data.weather[0].icon;
+          const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+          console.log("IMAGE NAME : ", description);
+          weatherInfo.innerHTML = `
+                <h2>${cityName}</h2>
+                <div>
+                    <img src="${iconUrl}" alt="${description}">
+                    <p>${temperature}&deg;C</p>
+                    <p>${description}</p>
+                </div>
+            `;
+        } else {
+          weatherInfo.innerHTML = `<p>Error: Unable to get weather data for ${cityName}</p>`;
         }
+      })
+      .catch((error) => {
+        weatherInfo.innerHTML = `<p>Error: ${error.message}</p>`;
+        console.log(error.message);
+      });
+
+    searchBox.addEventListener("keypress", function (event) {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        searchButton.click();
+      }
     });
     window.onbeforeunload = function () {
-        localStorage.clear();
-    }
-
+      localStorage.clear();
+    };
+  });
 };
+
